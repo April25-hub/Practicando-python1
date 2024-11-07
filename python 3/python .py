1d0980790e41bd3654ecffaeb3341bd5a26c271d
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 17:18:50 -0600

    Add files via upload

---
+++ python 3/python .py
@@ -0,0 +1,17 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+#esta linea definira las variables para el nombre y el apellido
+nombre = "April"  #esta linea la puedes modificar  estos valores para probar
+apellido = "Arzaba"  #esta linea te dira que puedes modificar estos valores para probar
+
+#esta linea comprobara si el nombre es April
+if nombre == "April":
+    #esta linea te dira si el nombre es April, comprobar el apellido
+    if apellido == "Arzaba":
+        print("Nombre y apellido correctos")
+    else:
+        print("Apellido incorrecto")
+else:
+    #esta linea dira si el nombre no es April, imprimir usuario desconocido
+    print("usuario desconocido")
