ff8495c6a921aee3bea47485c1a29a5debfeb526
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 16:29:24 -0600

    Add files via upload

---
+++ python 1/python.py
@@ -0,0 +1,13 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+#esta linea definira una variable con la edad
+edad = 15 #esta linea dira que puedes cambiar el valor de esta variable para probar diferentes edades
+
+#esta linea comprobara si la edad es mayor de 18
+if edad > 18:
+    #esta linea dira si la edad es mayor de 18 se imprime el mensaje correspondiente
+    print("eres mayor de edad")
+else:
+    #esta linea dira si la edad es 18 o menor y se imprime el otro mensaje
+    print("no eres mayor de edad")
