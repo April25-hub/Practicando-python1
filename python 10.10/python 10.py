fabf0f90284c1bb2e1fe3663bce7cdd80b9fc77c
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 20:31:43 -0600

    Add files via upload

---
+++ python 10.10/python 10.py
@@ -0,0 +1,16 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+#esta linea definira una lista con algunos elementos
+mi_lista = [10, 20, 30, 40, 50]
+
+#esta linea iniciamos el indice en 0, ya que vamos a recorrer la lista desde el primer elemento
+indice = 0
+
+#con esta linea usamos un bucle while para recorrer la lista
+while indice < len(mi_lista):
+    #con esta linea imprimimos el elemento actual de la lista
+    print(mi_lista[indice])
+    
+    #con esta linea aumentamos el indice para pasar al siguiente elemento
+    indice += 1
