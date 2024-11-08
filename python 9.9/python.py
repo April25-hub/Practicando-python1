11970f3fb416f8be3359ddc31c98df05c0d9bb38
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 20:20:47 -0600

    Add files via upload

---
+++ python 9.9/python.py
@@ -0,0 +1,10 @@
+print("Arzab Diaz April")
+print("1173")
+print("3W")
+#esta linea definira una lista con algunos elementos
+mi_lista = [1, 2, 3, 4, 5, "hola", "mundo"]
+
+#con esta linea uamos un bucle for para recorrer cada elemento de la lista
+for elemento in mi_lista:
+    #esta linea imprimimos cada elemento de la lista
+    print(elemento)
