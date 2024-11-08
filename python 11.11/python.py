40827b2b883fff9717df20669df8e6a33825cf3c
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 20:36:57 -0600

    Add files via upload

---
+++ python 11.11/python.py
@@ -0,0 +1,10 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+#con esta linea definimos una cadena de texto
+texto = "Hola Mundo"  #esta linea dara una cadena de texto que queremos procesar
+
+#esta linea nos dara un bucle 'for' para recorrer cada carácter en la cadena
+for caracter in texto:
+    #esta linea imprimir ael caracter actual
+    print(caracter)
