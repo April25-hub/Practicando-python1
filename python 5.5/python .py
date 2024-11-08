13bc5874891515b1e53f8f0c8de63712f67912ca
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 20:03:13 -0600

    Add files via upload

---
+++ python 5.5/python .py
@@ -0,0 +1,13 @@
+print("Arzaba Diaz April")
+print("1173")
+print("3W")
+#esta lista definira una lista de frutas
+frutas = ["manzana", "platano", "naranja", "pera", "fresa", "cereza", "uva"]
+
+#esta linea comprobara si la fruta "cereza" esta en la lista de frutas
+if "cereza" in frutas:
+    #esta linea dira si "cereza" esta en la lista, imprimir el mensaje correspondiente
+    print("cereza SI esta en la lista")
+else:
+    #esta linea dira si "cereza" no esta en la lista, imprimir el mensaje correspondiente
+    print("Cereza NO esta en la lista")
