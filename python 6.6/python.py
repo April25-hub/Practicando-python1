1690e90dd72763db866cae0678768177b7516f0d
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-07 20:08:11 -0600

    Add files via upload

---
+++ python 6.6/python.py
@@ -0,0 +1,12 @@
+print("Arzab Diaz April")
+print("1173")
+print("3W")
+#esta linea definira una lista con el nombre y los dos apellidos
+nombre_completo = ["April", "Arzaba", "Diaz"]
+
+#esta linea nos dejara acceder a los dos apellidos usando los indices de la lista
+primer_apellido = nombre_completo[1]  # el primer apellido esta en el indice 1
+segundo_apellido = nombre_completo[2]  # el segundo apellido esta en el indice 2
+
+#esta linea imprimira los apellidos
+print(primer_apellido, segundo_apellido)
